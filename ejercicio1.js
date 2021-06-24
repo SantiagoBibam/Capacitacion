05239575ff14fd392d2f090a444e1a9de1815e02
SantiagoBibam <bajero86@hotmail.com>
2021-06-24 14:12:57 -0300

    Modificacion

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,8 +1,13 @@
 var colores = ["blanco", "negro", "amarillo", "azul", "verde", "naranja"]
-var largoMaximo = ""
-for (let i=0; i<colores.length; i++){
-if (colores[i].length>largoMaximo.length){
-    largoMaximo = colores[i]
+
+var largoMaximoDeArray = function(array){
+    let largoMaximo = ""
+    for (let i=0; i<array.length; i++){
+        if (array[i].length>largoMaximo.length){
+            largoMaximo = colores[i]
+         }
+        
+    }
+    console.log(largoMaximo) 
 }
-}
-console.log(largoMaximo)
+largoMaximoDeArray(colores)
